e16716d76524bbea7c5fff6b3621b95624476b2d
quzzle <zhaolilong@catchapp.cc>
2017-04-14 12:25:22 +0800

    add file

---
+++ Test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test2
+//
+//  Created by zll on 2017/4/14.
+//  Copyright © 2017年 zll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test2
+//
+//  Created by zll on 2017/4/14.
+//  Copyright © 2017年 zll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
